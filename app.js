1b39350c715543249359eeec491a73b21dc00e9e
Luis Cardenas <info@luchohero.com>
2016-09-23 09:08:27 -0500

    Segundo Commit

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function(request, response) {
-  response.render('Hola Mundo');
+app.get('/', function (req, res) {
+  res.send('Hola Mundo');
 });
 
 app.listen(app.get('port'), function() {
